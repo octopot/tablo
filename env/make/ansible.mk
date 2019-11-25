2f849cfc242c3ce36e5525bf50a52e7248be0cc9
Kamil Samigullin <kamil@samigullin.info>
2019-11-25 22:45:03 +0300

    generalize ansible recipes in makefile

--- env/make/ansible.mk
+++ env/make/ansible.mk
@@ -1,30 +1,30 @@
 _playbook = ansible-playbook -i env/ansible/.private.hosts
-_recipes  = env/ansible/ubuntu.yml env/ansible/docker.yml env/ansible/service.yml
+_recipes  = ubuntu docker service
 
 .PHONY: ansible-playbook-apply
+ansible-playbook-apply: recipes=$(foreach recipe,$(_recipes),env/ansible/$(recipe).yml)
 ansible-playbook-apply:
-	@$(_playbook) $(_recipes)
+	$(_playbook) $(recipes)
 
 .PHONY: ansible-playbook-check
+ansible-playbook-check: recipes=$(foreach recipe,$(_recipes),env/ansible/$(recipe).yml)
 ansible-playbook-check:
-	@$(_playbook) --check $(_recipes)
+	$(_playbook) --check $(recipes)
 
 .PHONY: ansible-playbook-diff
+ansible-playbook-diff: recipes=$(foreach recipe,$(_recipes),env/ansible/$(recipe).yml)
 ansible-playbook-diff:
-	@$(_playbook) --diff $(_recipes)
+	$(_playbook) --diff $(recipes)
 
 
-.PHONY: ansible-playbook-for-ubuntu
-ansible-playbook-for-ubuntu:
-	$(eval _recipes = env/ansible/ubuntu.yml)
-	@echo recipe $(_recipes) will be used
+define recipe_tpl
 
-.PHONY: ansible-playbook-for-docker
-ansible-playbook-for-docker:
-	$(eval _recipes = env/ansible/docker.yml)
-	@echo recipe $(_recipes) will be used
+.PHONY: ansible-playbook-for-$(1)
+ansible-playbook-for-$(1):
+	$$(eval _recipes = $(1))
+	@echo recipe env/ansible/$$(_recipes).yml will be used
 
-.PHONY: ansible-playbook-for-service
-ansible-playbook-for-service:
-	$(eval _recipes = env/ansible/service.yml)
-	@echo recipe $(_recipes) will be used
+endef
+
+render_recipe_tpl = $(eval $(call recipe_tpl,$(recipe)))
+$(foreach recipe,$(_recipes),$(render_recipe_tpl))
